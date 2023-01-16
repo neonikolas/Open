a265b2892a1c8b2d5bfbf51a8890e182e720479c
Neonikolas <191287kzn@mail.ru>
2023-01-17 00:30:03 +0300

    контр

--- контр.py
+++ контр.py
@@ -1,10 +1,10 @@
 per_cent = {'ТБК': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("Вводите сумму, которую желаете положить под проценты:"))
 
-TBK = int((per_cent['ТБК']) * (money/10))
-SKB = int((per_cent['СКБ']) * (money/10))
-VTB = int((per_cent['ВТБ']) * (money/10))
-SBER = int((per_cent['СБЕР']) * (money/10))
+TBK = int((per_cent['ТБК']) * (money/100))
+SKB = int((per_cent['СКБ']) * (money/100))
+VTB = int((per_cent['ВТБ']) * (money/100))
+SBER = int((per_cent['СБЕР']) * (money/100))
 
 deposit = [TBK, SKB, VTB, SBER]
 print("Накопленные средства за год в каждом из банков =", deposit)
